d65b49138f3a7ce1daef15ba39b67467bf426d47
Neida Caripa <neida.caripa@gmail.com>
2024-01-09 15:37:52 -0500

    About

---
+++ src/Pages/about/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const About = () => {
+  return (
+    <div>
+        <span>Quiénes somos?</span>
+        <span>Historia</span>
+        <p>Learning time fue creado en el año 2023 en la ciudad de Medellín por iniciativa de un grupo de líderes Makaia la cual se materializó inicialmente como una organización de estudios. Dicho propósito busca satisfacer las necesidades evidenciadas en los estudiantes, así fue como se formuló un proyecto educativo institucional, con el cuál nació LEARNING TIME, se inicia para ofrecer los siguientes cursos:</p>
+<ul>•	Gestión del tiempo
+<li>Nutrición y salud</li>	
+<li>Salud mental</li>
+<li>Inteligencia emocional</li>
+<li>Educación financiera</li>
+<li>Programación y desarrollo web</li>
+</ul>
+<p>Los estudiantes, como principal público, encontrarán en nuestra aplicación un amplio contenido especialmente diseñado para su beneficio.
+Nuestro enfoque se centra en brindar soluciones para aquellos que buscan superar obstáculos y mejorar su bienestar en su viaje educativo y personal.</p>
+
+<p>En el contexto de soluciones similares, existen proyectos que abordan aspectos relacionados con el bienestar emocional y el desarrollo de habilidades en estudiantes.</p>
+
+<p>Sin embargo, nuestra propuesta se distingue por su enfoque integral en la educación, ofreciendo una plataforma que no solo se centra en la salud mental, sino que también abarca áreas clave como el manejo del dinero y otros aspectos fundamentales para el crecimiento holístico de los estudiantes. Esta combinación de contenido diverso y especializado es lo que diferencia y enriquece nuestra plataforma.</p>
+  </div>
+  )}
+  export default About
